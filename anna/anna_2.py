31f67c99f7d32d805e0151a63af4622cbf793565
MikhalevaAnna <mikhaleva.ag@gmail.com>
2025-10-16 20:15:16 +0300

    Внесла изменения в строки 6 и 7 файла anna_2.py

--- anna/anna_2.py
+++ anna/anna_2.py
@@ -3,8 +3,8 @@ from itertools import groupby
 
 Person = namedtuple('Person', ['name', 'age', 'height'])
 persons = [Person('Timosha', 63, 193), Person('Eva', 47, 158),
-           Person('Barsuk', 71, 172), Person('Alex', 45, 193),
-           Person('Jeff', 63, 193), Person('Ryanold', 41, 184),
+           Person('Barsuсhok', 71, 172), Person('Alex', 45, 193),
+           Person('Jeffry', 63, 193), Person('Ryanold', 41, 184),
            Person('Markus', 71, 172), Person('Alexey', 45, 193),
            Person('Jeffry', 63, 193), Person('Ryanold', 41, 184),
            Person('Arianatt', 28, 158), Person('Liam', 69, 193),
